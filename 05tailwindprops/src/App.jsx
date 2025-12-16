a6f719dff3a2980a82317546703c24caae4d7869
sonamverma28045@gmail.com <sonamverma28045@gmail.com>
2025-12-17 01:00:41 +0530

    Today I learned how to pass props inside a component and how to pass default value to props.

--- 05tailwindprops/src/App.jsx
+++ 05tailwindprops/src/App.jsx
@@ -5,12 +5,17 @@ import './App.css'
 import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
-
+  let myObj = {
+    username: "sonam",
+    age: 23
+  }
+  let newArr = [1, 2, 3]
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
-      <Card/>
-      <Card/>
+      <Card username="Sonam" someObj= {myObj} btnText= "Visit me"/>
+      <Card username= "Khushi" someObj= {newArr} btnText = "Click Me"/>
+      <Card username= "Khushi" someObj= {newArr} /> {/*default value of btnText is passed in Card.jsx*/}
     </>
   )
 }
